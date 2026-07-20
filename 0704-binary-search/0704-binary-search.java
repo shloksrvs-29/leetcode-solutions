f0aa81b1a7e4617aaa81a35ab4f3a8492dc1e37e
Shlok Srivastava <shlok.srvs@gmail.com>
2026-07-20 23:19:09 +0530

    Time: 0 ms (100%), Space: 48.1 MB (85.34%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        return binarySearch(nums, 0, nums.length - 1, target);
+    }
+
+    private int binarySearch(int[] nums, int low, int high, int target) {
+        if (low > high) {
+            return -1;
+        }
+
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            return binarySearch(nums, low, mid - 1, target);
+        } else {
+            return binarySearch(nums, mid + 1, high, target);
+        }
+    }
+}
