5b5104082fca765086556bf900649da34d776d27
Shlok Srivastava <shlok.srvs@gmail.com>
2026-08-31 15:56:12 +0530

    Time: 8 ms (91.21%), Space: 46 MB (94.07%) - LeetHub

---
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isValid(String s) {
+        while (s.contains("abc")) {
+            s = s.replace("abc", "");
+        }
+
+        return s.length() == 0;
+    }
+}
