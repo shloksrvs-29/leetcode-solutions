7f3252bdb57cdf00219e67f2889377e9b86031c2
Shlok Srivastava <shlok.srvs@gmail.com>
2026-09-24 12:11:27 +0530

    Time: 19 ms (48.31%), Space: 108 MB (57.11%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set= new HashSet<>();
+        for (int ele: nums)
+        {
+            if (set.contains(ele))
+            {
+                return true;
+            }
+            set.add(ele);
+        }
+        return false;
+    }
+}
